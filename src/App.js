34b64e43853e13ca638fb0afc81d42727b7d3008
Arvind <arvind@users.noreply.github.com>
2020-08-26 19:50:00 +0530

    Node modules update
    Node modules update  - After git check in few files in .bin folder were missing, adding the missing files.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
-import arvika from './Arvika.png';
+//import arvika from './Arvika.png';
 import './App.css';
 import Greet from './Greet';
 import Welcome from './Welcome';
 import EventBinder from './EventBinding';
 import ParentComponent from './ParentComponent';
 import ConditionalCarPurchase from './ConditionalOperators';
+// import List from './ListInReact';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         {/* <EventBinder /> */}
         {/* <ParentComponent /> */}
         <ConditionalCarPurchase/>
+        {/* <List /> */}
 
 
       </header>
